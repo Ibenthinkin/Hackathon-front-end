f89e4a8c5ea17788bea59e9ee86e0886f69869a6
ibenthinkin <benjamin.reilly@gmail.com>
2019-05-21 13:57:53 -0600

    add a gif and randomier

--- cleanapp/src/Card.js
+++ cleanapp/src/Card.js
@@ -4,17 +4,18 @@ import "./Card.css"
 
 class Card extends React.Component {
   state ={
-    completed:false
+    completed:false,
   }
 
   handleChange = event => {
-    
   }
+
 
   render(){
   return(
     <div className='user-card hvr-grow '>
       <h3>{this.props.description}</h3>
+      <img className="gif" src='https://media.tenor.com/images/ff4f31cd5064722ed52609069653fe77/tenor.gif' alt=''/>
       <div className='points-div'>
       <img className='card-tree'src={require(`./images/tree.png`)}></img>
       <p>x{this.props.points}</p>
--- cleanapp/src/CardContainer.js
+++ cleanapp/src/CardContainer.js
@@ -4,11 +4,17 @@ import './Container.css'
 
 const Container = props => {
 
-  const cards = props.cards.map((card, index) => <Card {...card} key={Date.now() + index} />)
+  const cards = props.cards.map((card, index) => <Card {...card} key={card.id} />)
+
+  let sixCards = cards.sort(() => .5 - Math.random()).slice(0,6)
+
+
+
+
 
   return(
     <div className="card">
-      {cards}
+      {sixCards}
     </div>
   )
 }
